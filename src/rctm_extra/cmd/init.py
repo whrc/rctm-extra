bb9d777bf4bbb70dc6954dd9fa1f8d82bb99a28c
Dogukan Teber <dogukanteber1@hotmail.com>
2025-05-05 16:09:57 +0000

    Implement the init method

--- src/rctm_extra/cmd/init.py
+++ src/rctm_extra/cmd/init.py
@@ -1,9 +1,27 @@
+import subprocess
+import os
+
 from rctm_extra.cmd.base import BaseCommand
 
 
 class InitCommand(BaseCommand):
+    __RCTM_REPOSITORY = "https://github.com/whrc/RCTM.git"
+
     def __init__(self, args):
         super().__init__(args)
 
+    def _clone_repository(self, repo_url: str, target_path: str):
+        try:
+            subprocess.run(f"git clone {repo_url} {target_path}", shell=True, check=True)
+            print(f"Successfully cloned RCTM repository to {target_path}")
+        except subprocess.CalledProcessError as e:
+            print(f"Error cloning repository: {e}")
+            raise
+
     def execute(self):
-        print("init command")
+        rctm_target_dir = os.path.expanduser("~/RCTM")
+        if not os.path.exists(rctm_target_dir):
+            print("Couldn't find RCTM in the home folder. Cloning it...")
+            self._clone_repository(self.__RCTM_REPOSITORY, rctm_target_dir)
+
+        print("\n\nYou're all set!")
